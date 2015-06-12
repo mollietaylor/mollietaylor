b77e3fa955a56d8cf034919b955155e1a075baea
mollietaylor <mollie.taylor@gmail.com>
2015-06-11 20:39:16 -0400

    Starts new website

---
+++ README.md
@@ -0,0 +1,6 @@
+favicon
+font(s)
+get links working
+top bar color
+size images based on rows & cols
+max-size of images based on image
--- app.js
+++ app.js
@@ -1,8 +1,108 @@
-var app = angular.module('MySite', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
- 
-}]);
+angular
+  .module('MyApp', ['ngMaterial'])
+  .controller('gridListDemoCtrl', function($scope) {
+
+    this.tiles = [];
+    	
+  	this.tiles.push({"icon": "TwitterLogo_white.png",
+  									 "title": "@mollie_taylor",
+  									 "link": "https://twitter.com/mollie_taylor",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "#55acee"});
+    	
+  	this.tiles.push({"icon": "maptime.png",
+  									 "title": "Co-Organizer, MaptimeATL!",
+  									 "link": "http://www.meetup.com/MaptimeATL/",
+  									 "span": {"col": 2, "row": 1},
+  									 "background": "#fff"});
+    	
+  	this.tiles.push({"icon": "codeforatlanta.png",
+  									 "title": "Co-Organizer, Code for Atlanta",
+  									 "link": "http://www.codeforatlanta.org/",
+  									 "span": {"col": 2, "row": 2},
+  									 "background": "#fff"});
+    	
+  	this.tiles.push({"icon": "proximity-doo-small.jpg",
+  									 "title": "Art Project: Proximity To Nature",
+  									 "link": "http://proximity2nature.tumblr.com/",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "#202020"});
+    	
+  	this.tiles.push({"icon": "GitHub-Mark-Light-120px-plus.png",
+  									 "title": "mollietaylor",
+  									 "link": "https://github.com/mollietaylor",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "#000"});
+    	
+  	this.tiles.push({"icon": "prox-mollie1.png",
+  									 "title": "President, Proximity Viz LLC",
+  									 "link": "http://proximityviz.com/",
+  									 "span": {"col": 2, "row": 2},
+  									 "background": "#fff"});
+    	
+  	this.tiles.push({"icon": "Public-images-osm_logo.png",
+  									 "title": "mollietaylor",
+  									 "link": "https://www.openstreetmap.org/user/mollietaylor/history",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "blog.png",
+  									 "title": 'Blog: "Maps and Apps"',
+  									 "link": "http://blog.mollietaylor.com/",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "#f5f5f5"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "mollie.taylor@gmail.com",
+  									 "link": "mailto:mollie.taylor@gmail.com",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "molliemtaylor",
+  									 "link": "https://instagram.com/molliemtaylor/",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+    	
+  	this.tiles.push({"icon": "",
+  									 "title": "",
+  									 "link": "",
+  									 "span": {"col": 1, "row": 1},
+  									 "background": "green"});
+
+    console.log(this.tiles);
+  })
+  .config( function( $mdIconProvider ){
+    $mdIconProvider.iconSet("avatar", 'icons/avatar-icons.svg', 128);
+  });
